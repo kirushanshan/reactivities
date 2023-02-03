711e39508fc9e6a67879d245cb85ebcdfc769d36
kirushanshan <kirushan93@gmail.com>
2023-02-03 11:59:38 +0530

    CancellationToken Unimplement

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -24,20 +24,6 @@ namespace Application.Activities
 
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
-                try
-                {
-                    for(var i=0; i<10; i++)
-                    {
-                        cancellationToken.ThrowIfCancellationRequested();
-                        await Task.Delay(1000, cancellationToken);
-                        _logger.LogInformation($"Task {i} has completed");
-                    }
-                }
-                catch (System.Exception)
-                {
-                    
-                    _logger.LogInformation("Task Was Cancelled");
-                }
                 return await _context.Activities.ToListAsync();
             }
         }
